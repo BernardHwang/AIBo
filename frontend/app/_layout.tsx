a4aad0ebfcc5475c6853a489ee97f300a80ec9ce
BernardHwang <laohwang0523@gmail.com>
2025-07-16 10:04:31 +0800

    Development: Added font

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,32 +1,41 @@
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import { ThemeProvider, useAppTheme } from '@/components//ThemeContext';
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { ThemeProvider, useAppTheme } from "@/components//ThemeContext";
+import {
+    useFonts,
+    Inter_400Regular,
+    Inter_800ExtraBold,
+} from "@expo-google-fonts/inter";
 
 export default function RootLayout() {
-  return (
-    <SafeAreaProvider>
-        <ThemeProvider>
-            <ThemedWrapper />
-        </ThemeProvider>
-    </SafeAreaProvider>
-  );
+    return (
+        <SafeAreaProvider>
+            <ThemeProvider>
+                <ThemedWrapper />
+            </ThemeProvider>
+        </SafeAreaProvider>
+    );
 }
 
 function ThemedWrapper() {
-  const { theme, mode }: any = useAppTheme();
+    const { theme, mode }: any = useAppTheme();
+    const [fontsLoaded] = useFonts({
+        Inter_400Regular,
+        Inter_800ExtraBold
+    });
 
-  return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: theme.background }}>
-      <StatusBar style={mode === 'dark' ? 'light' : 'dark'} />
-      <Stack
-        screenOptions={{
-          headerShown: false,
-          contentStyle: {
-            backgroundColor: theme.background
-          },
-        }}
-      />
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: theme.background }}>
+            <StatusBar style={mode === "dark" ? "light" : "dark"} />
+            <Stack
+                screenOptions={{
+                    headerShown: false,
+                    contentStyle: {
+                        backgroundColor: theme.background,
+                    },
+                }}
+            />
+        </SafeAreaView>
+    );
 }
